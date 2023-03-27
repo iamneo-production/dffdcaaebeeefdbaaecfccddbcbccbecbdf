05f6d6bee696299b4886fc7974bdb8d908a2d190
root <root@dffdcaaebeeefdbaaecfccddbcbccbecbdf-0.dffdcaaebeeefdbaaecfccddbcbccbecbdf.che.svc.cluster.local>
2023-03-27 04:47:25 +0000

    Backup Branch before deleting

---
+++ react/tests/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('test_case01', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Let we see the output of JAVASCRIPT/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('test_case02',()=>
+{
+  render(<App/>);
+  const myElement=screen.getByRole('button');
+  expect(myElement).toBeInTheDocument();
+});
